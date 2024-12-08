9b92d4117d67122c0f89f31791234eb5f84a3d5e
litcheed <litcheed@gmail.com>
2024-12-08 13:58:43 +0900

    fix: CSR化 要修正

--- blog_client/app/page.tsx
+++ blog_client/app/page.tsx
@@ -1,6 +1,31 @@
-export default async function Home() {
-  const res = await fetch("http://localhost:3001/api/v1/posts");
-  const posts = await res.json();
+'use client';
+
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function  fetchPosts() {
+      try {
+        const res = await fetch("http://localhost:3001/api/v1/posts");
+        // if (!res.ok) {
+        //   throw new Error(`Failed to fetch posts: ${res.status}`);
+        // }
+        const data = await res.json();
+        setPosts(data);
+      } catch(error) {
+        console.error(error)
+      }
+    }
+    
+    fetchPosts();
+  }, []);
+
+
+  function deletePost(id: number) {
+    console.log("削除実行")
+  }
   
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -11,6 +36,7 @@ export default async function Home() {
               <div>{post.title}</div>
               <div>{post.content}</div>
               <div>{post.updated_at}</div>
+              <button onClick={()=>deletePost(post.id)}></button>
             </li>
           ))}
         </ul>
